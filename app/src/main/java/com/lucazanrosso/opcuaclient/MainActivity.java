1009f9417627663be680f5a10d0153af6b18c21e
lucazanrosso <lucazanrosso94@gmail.com>
2018-04-02 12:24:37 +0200

    Connect to server without using Prosys sdk

--- app/src/main/java/com/lucazanrosso/opcuaclient/MainActivity.java
+++ app/src/main/java/com/lucazanrosso/opcuaclient/MainActivity.java
@@ -1,111 +1,113 @@
 package com.lucazanrosso.opcuaclient;
 
-import android.app.Activity;
 import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
-import com.prosysopc.ua.ApplicationIdentity;
-import com.prosysopc.ua.CertificateValidationListener;
-import com.prosysopc.ua.PkiFileBasedCertificateValidator;
-import com.prosysopc.ua.UserIdentity;
-import com.prosysopc.ua.client.UaClient;
+import java.security.Security;
 
-import org.opcfoundation.ua.builtintypes.DataValue;
-import org.opcfoundation.ua.builtintypes.LocalizedText;
-import org.opcfoundation.ua.core.ApplicationDescription;
-import org.opcfoundation.ua.core.ApplicationType;
-import org.opcfoundation.ua.core.Identifiers;
-import org.opcfoundation.ua.transport.security.Cert;
-import org.opcfoundation.ua.transport.security.SecurityMode;
+import org.opcfoundation.ua.application.Application;
+import org.opcfoundation.ua.application.Client;
+import org.opcfoundation.ua.core.EndpointDescription;
+import org.opcfoundation.ua.transport.ServiceChannel;
+import org.opcfoundation.ua.transport.security.KeyPair;
+import org.opcfoundation.ua.utils.CertificateUtils;
+import org.opcfoundation.ua.utils.EndpointUtil;
 
-import java.io.File;
-import java.util.EnumSet;
-import java.util.Locale;
-
-public class MainActivity extends Activity {
-    Button connectButton;
-    TextView textView;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Security.insertProviderAt(new org.spongycastle.jce.provider.BouncyCastleProvider(), 1);
 
-        connectButton = findViewById(R.id.connectButton);
-        textView = findViewById(R.id.textView);
-
-        connectButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ConnectAndReadTask task = new ConnectAndReadTask();
-                task.execute();
-            }
-        });
     }
 
-    private class ConnectAndReadTask extends AsyncTask<Void, Void, String> {
+    public void connect(View view) {
+        new  Connect().execute(null, null, null);
+    }
 
-        public ConnectAndReadTask() {
-        }
+    private static class Connect extends AsyncTask<String, String, String> {
 
         @Override
-        protected String doInBackground(Void... params) {
-            UaClient client;
-            String result;
+        protected String doInBackground(String... params) {
+
             try {
-                client = new UaClient("");
+                ////////////// CLIENT //////////////
+                Application myClientApplication = new Application();
+                // Load Client's Application Instance Certificate from file
+                KeyPair myClientApplicationInstanceCertificate = null;
 
-                ApplicationDescription appDescription = new ApplicationDescription();
-                appDescription.setApplicationName(new LocalizedText("SimpleAndroidClient",
-                        Locale.ENGLISH));
-                String android_id = Settings.Secure.getString(
-                        getApplicationContext().getContentResolver(),
-                        Settings.Secure.ANDROID_ID);
-                appDescription.setApplicationUri("urn:" + android_id +":UA:SimpleAndroidClient");
-                appDescription.setProductUri("urn:prosysopc.com:UA:SimpleAndroidClient");
-                appDescription.setApplicationType(ApplicationType.Client);
+                // Create Client Application Instance Certificate
+                {
+                    String certificateCommonName = "UA Sample Client";
+                    System.out.println("Generating new Certificate for Client using CN: " + certificateCommonName);
+                    String applicationUri = myClientApplication.getApplicationUri();
+                    String organisation = "Sample Organisation";
+                    int validityTime = 365;
+                    myClientApplicationInstanceCertificate = CertificateUtils
+                            .createApplicationInstanceCertificate(certificateCommonName, organisation, applicationUri, validityTime);
+                }
 
-                PkiFileBasedCertificateValidator validator = new PkiFileBasedCertificateValidator(
-                        getFilesDir().getPath() + "/PKI/CA");
-                validator.setValidationListener(new CertificateValidationListener() {
-                    @Override
-                    public PkiFileBasedCertificateValidator.ValidationResult onValidate(
-                            Cert cert, ApplicationDescription applicationDescription,
-                            EnumSet<PkiFileBasedCertificateValidator.CertificateCheck> enumSet) {
-                        return PkiFileBasedCertificateValidator.ValidationResult.AcceptPermanently;
-                    }
-                });
-                client.setCertificateValidator(validator);
+                // Create Client
+                Client myClient = new Client(myClientApplication);
+                myClientApplication.addApplicationInstanceCertificate(myClientApplicationInstanceCertificate);
+                //////////////////////////////////////
 
-                ApplicationIdentity identity = ApplicationIdentity.loadOrCreateCertificate(
-                        appDescription, "Sample Organisation", "opcua",
-                        new File(validator.getBaseDir(), "private"), true);
+                /////////// DISCOVER ENDPOINT ////////
+                // Discover server's endpoints, and choose one
+                EndpointDescription[] endpoints = myClient.discoverEndpoints(""); // 51210=Sample
+                // Server
+                // Filter out all but opc.tcp protocol endpoints
+                endpoints = EndpointUtil.selectByProtocol(endpoints, "opc.tcp");
+                // Filter out all but Signed & Encrypted endpoints
+//                endpoints = EndpointUtil.selectByMessageSecurityMode(endpoints, MessageSecurityMode.SignAndEncrypt);
+//                // Filter out all but Basic128 cryption endpoints
+//                endpoints = EndpointUtil.selectBySecurityPolicy(endpoints, SecurityPolicy.BASIC128RSA15);
+                // Sort endpoints by security level. The lowest level at the beginning, the highest at the end
+                // of the array
+                endpoints = EndpointUtil.sortBySecurityLevel(endpoints);
+                // Choose one endpoint
+                EndpointDescription endpoint = endpoints[endpoints.length - 1];
+                //////////////////////////////////////
 
-                identity.setApplicationDescription(appDescription);
-                client.setApplicationIdentity(identity);
+                //////////// Open and close connection ////////////
+                // Create Channel
+                ServiceChannel myChannel = myClient.createServiceChannel(endpoint);
+                System.out.println("Connected to server at '" + endpoint.getEndpointUrl() + "' with Security Mode '"
+                        + endpoint.getSecurityMode() + "' and Security Policy '" + endpoint.getSecurityPolicyUri() + "'");
+//                Toast.makeText(context, "Connected to server at '" + endpoint.getEndpointUrl() + "' with Security Mode '"
+//                        + endpoint.getSecurityMode() + "' and Security Policy '" + endpoint.getSecurityPolicyUri() + "'", Toast.LENGTH_LONG).show();
+                ///////////// SHUTDOWN /////////////
+                // Close channel
+                myChannel.closeAsync();
 
-                client.setTimeout(60000);
-                client.setSecurityMode(SecurityMode.NONE);
-                client.setUserIdentity(new UserIdentity());
-                client.connect();
-
-                DataValue dv = client.readValue(Identifiers.Server_ServerStatus_CurrentTime);
-                result = (dv.getValue().getValue()).toString();
-
-                client.disconnect();
             } catch (Exception e) {
-                result = e.toString();
+                e.printStackTrace();
             }
-            return result;
+
+            return "Connecting...";
         }
 
+
         @Override
-        protected void onPostExecute(String s) {
-            textView.setText(s);
+        protected void onPostExecute(String result) {
+
+        }
+
+
+        @Override
+        protected void onPreExecute() {
+
+        }
+
+
+        @Override
+        protected void onProgressUpdate(String... text) {
+
         }
     }
 }
