bbc5bfbd9412e2dd92f09d41868b05ae604d4558
lucazanrosso <lucazanrosso94@gmail.com>
2018-04-04 00:00:59 +0200

    Read a value from a node

--- app/src/main/java/com/lucazanrosso/opcuaclient/MainActivity.java
+++ app/src/main/java/com/lucazanrosso/opcuaclient/MainActivity.java
@@ -6,10 +6,22 @@ import android.os.Bundle;
 import android.view.View;
 
 import java.security.Security;
+import java.util.Arrays;
 
 import org.opcfoundation.ua.application.Application;
 import org.opcfoundation.ua.application.Client;
+import org.opcfoundation.ua.application.SessionChannel;
+import org.opcfoundation.ua.builtintypes.NodeId;
+import org.opcfoundation.ua.core.Attributes;
+import org.opcfoundation.ua.core.BrowseDescription;
+import org.opcfoundation.ua.core.BrowseDirection;
+import org.opcfoundation.ua.core.BrowseResponse;
 import org.opcfoundation.ua.core.EndpointDescription;
+import org.opcfoundation.ua.core.Identifiers;
+import org.opcfoundation.ua.core.ReadRequest;
+import org.opcfoundation.ua.core.ReadResponse;
+import org.opcfoundation.ua.core.ReadValueId;
+import org.opcfoundation.ua.core.TimestampsToReturn;
 import org.opcfoundation.ua.transport.ServiceChannel;
 import org.opcfoundation.ua.transport.security.KeyPair;
 import org.opcfoundation.ua.utils.CertificateUtils;
@@ -59,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
 
                 /////////// DISCOVER ENDPOINT ////////
                 // Discover server's endpoints, and choose one
-                EndpointDescription[] endpoints = myClient.discoverEndpoints(""); // 51210=Sample
+                EndpointDescription[] endpoints = myClient.discoverEndpoints("opc.tcp://DESKTOP-EGB7B8G.homenet.telecomitalia.it:53530/OPCUA/SimulationServer"); // 51210=Sample
                 // Server
                 // Filter out all but opc.tcp protocol endpoints
                 endpoints = EndpointUtil.selectByProtocol(endpoints, "opc.tcp");
@@ -71,19 +83,84 @@ public class MainActivity extends AppCompatActivity {
                 // of the array
                 endpoints = EndpointUtil.sortBySecurityLevel(endpoints);
                 // Choose one endpoint
-                EndpointDescription endpoint = endpoints[endpoints.length - 1];
+                EndpointDescription endpoint = endpoints[0];
                 //////////////////////////////////////
 
+
+
                 //////////// Open and close connection ////////////
+//                Create Channel
+//                ServiceChannel myChannel = myClient.createServiceChannel(endpoint);
+
+                //////////// TEST-STACK ////////////
                 // Create Channel
-                ServiceChannel myChannel = myClient.createServiceChannel(endpoint);
-                System.out.println("Connected to server at '" + endpoint.getEndpointUrl() + "' with Security Mode '"
-                        + endpoint.getSecurityMode() + "' and Security Policy '" + endpoint.getSecurityPolicyUri() + "'");
-//                Toast.makeText(context, "Connected to server at '" + endpoint.getEndpointUrl() + "' with Security Mode '"
-//                        + endpoint.getSecurityMode() + "' and Security Policy '" + endpoint.getSecurityPolicyUri() + "'", Toast.LENGTH_LONG).show();
-                ///////////// SHUTDOWN /////////////
-                // Close channel
-                myChannel.closeAsync();
+//                ServiceChannel myChannel = myClient.createServiceChannel(endpoint);
+//                // Create Test Request
+//                ReadValueId[] nodesToRead = {new ReadValueId(Identifiers.RootFolder, Attributes.BrowseName, null, null)};
+//                ReadRequest req = new ReadRequest(null, 0.0, TimestampsToReturn.Both, nodesToRead);
+//                System.out.println("REQUEST: " + req);
+//
+//                // Invoke service
+//                ReadResponse res = myChannel.Read(req);
+//                // Print result
+//                System.out.println("RESPONSE: " + res);
+//                //////////////////////////////////////
+//
+//
+//                System.out.println("Connected to server at '" + endpoint.getEndpointUrl() + "' with Security Mode '"
+//                        + endpoint.getSecurityMode() + "' and Security Policy '" + endpoint.getSecurityPolicyUri() + "'");
+//
+//                ///////////// SHUTDOWN /////////////
+////                 Close channel
+//                myChannel.closeAsync();
+
+
+
+                SessionChannel mySession = myClient.createSessionChannel(endpoint);
+                // mySession.activate("username", "123");
+                mySession.activate();
+
+                // Read a variable
+                ReadResponse res4 = mySession.Read(null, 500.0, TimestampsToReturn.Source,
+                        new ReadValueId(new NodeId(5, "Counter1"), Attributes.Value, null, null));
+                System.out.println(res4);
+
+
+                // Other examples to test
+                // 3333333
+                // Browse Root
+//                BrowseDescription browse = new BrowseDescription();
+//                browse.setNodeId(Identifiers.RootFolder);
+//                browse.setBrowseDirection(BrowseDirection.Forward);
+//                browse.setIncludeSubtypes(true);
+//                BrowseResponse res3 = mySession.Browse(null, null, null, browse);
+//                System.out.println(res3);
+
+                // 55555555
+                // Read Namespace Array
+//                ReadResponse res5 = mySession.Read(null, null, TimestampsToReturn.Neither,
+//                        new ReadValueId(Identifiers.Server_NamespaceArray, Attributes.Value, null, null));
+//                String[] namespaceArray = (String[]) res5.getResults()[0].getValue().getValue();
+//                System.out.println(Arrays.toString(namespaceArray));
+
+                //////////// TEST-STACK ////////////
+                // Create Channel
+//                ServiceChannel myChannel = myClient.createServiceChannel(endpoint);
+//                // Create Test Request
+//                ReadValueId[] nodesToRead = {new ReadValueId(Identifiers.RootFolder, Attributes.BrowseName, null, null)};
+//                ReadRequest req = new ReadRequest(null, 0.0, TimestampsToReturn.Both, nodesToRead);
+//                System.out.println("REQUEST: " + req);
+//
+//                // Invoke service
+//                ReadResponse res = myChannel.Read(req);
+//                // Print result
+//                System.out.println("RESPONSE: " + res);
+                //////////////////////////////////////
+
+
+                mySession.close();
+                mySession.closeAsync();
+
 
             } catch (Exception e) {
                 e.printStackTrace();
