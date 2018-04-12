6c1d5c0df771b17f596d42447859fbdb4a33cc04
lucazanrosso <lucazanrosso94@gmail.com>
2018-04-12 19:37:06 +0200

    Write a value to a node

--- app/src/main/java/com/lucazanrosso/opcuaclient/MainActivity.java
+++ app/src/main/java/com/lucazanrosso/opcuaclient/MainActivity.java
@@ -6,12 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 
 import java.security.Security;
-import java.util.Arrays;
 
 import org.opcfoundation.ua.application.Application;
 import org.opcfoundation.ua.application.Client;
 import org.opcfoundation.ua.application.SessionChannel;
+import org.opcfoundation.ua.builtintypes.DataValue;
 import org.opcfoundation.ua.builtintypes.NodeId;
+import org.opcfoundation.ua.builtintypes.Variant;
 import org.opcfoundation.ua.core.Attributes;
 import org.opcfoundation.ua.core.BrowseDescription;
 import org.opcfoundation.ua.core.BrowseDirection;
@@ -22,6 +23,7 @@ import org.opcfoundation.ua.core.ReadRequest;
 import org.opcfoundation.ua.core.ReadResponse;
 import org.opcfoundation.ua.core.ReadValueId;
 import org.opcfoundation.ua.core.TimestampsToReturn;
+import org.opcfoundation.ua.core.WriteValue;
 import org.opcfoundation.ua.transport.ServiceChannel;
 import org.opcfoundation.ua.transport.security.KeyPair;
 import org.opcfoundation.ua.utils.CertificateUtils;
@@ -86,81 +88,20 @@ public class MainActivity extends AppCompatActivity {
                 EndpointDescription endpoint = endpoints[0];
                 //////////////////////////////////////
 
-
-
-                //////////// Open and close connection ////////////
-//                Create Channel
-//                ServiceChannel myChannel = myClient.createServiceChannel(endpoint);
-
-                //////////// TEST-STACK ////////////
-                // Create Channel
-//                ServiceChannel myChannel = myClient.createServiceChannel(endpoint);
-//                // Create Test Request
-//                ReadValueId[] nodesToRead = {new ReadValueId(Identifiers.RootFolder, Attributes.BrowseName, null, null)};
-//                ReadRequest req = new ReadRequest(null, 0.0, TimestampsToReturn.Both, nodesToRead);
-//                System.out.println("REQUEST: " + req);
-//
-//                // Invoke service
-//                ReadResponse res = myChannel.Read(req);
-//                // Print result
-//                System.out.println("RESPONSE: " + res);
-//                //////////////////////////////////////
-//
-//
-//                System.out.println("Connected to server at '" + endpoint.getEndpointUrl() + "' with Security Mode '"
-//                        + endpoint.getSecurityMode() + "' and Security Policy '" + endpoint.getSecurityPolicyUri() + "'");
-//
-//                ///////////// SHUTDOWN /////////////
-////                 Close channel
-//                myChannel.closeAsync();
-
-
-
                 SessionChannel mySession = myClient.createSessionChannel(endpoint);
-                // mySession.activate("username", "123");
-                mySession.activate();
+                mySession.activate("Luca", "lukesky");
+//                mySession.activate();
 
-                // Read a variable
-                ReadResponse res4 = mySession.Read(null, 500.0, TimestampsToReturn.Source,
+
+////                // Read a variable
+                ReadResponse res = mySession.Read(null, 500.0, TimestampsToReturn.Source,
                         new ReadValueId(new NodeId(5, "Counter1"), Attributes.Value, null, null));
-                System.out.println(res4);
+                System.out.println(res);
 
-
-                // Other examples to test
-                // 3333333
-                // Browse Root
-//                BrowseDescription browse = new BrowseDescription();
-//                browse.setNodeId(Identifiers.RootFolder);
-//                browse.setBrowseDirection(BrowseDirection.Forward);
-//                browse.setIncludeSubtypes(true);
-//                BrowseResponse res3 = mySession.Browse(null, null, null, browse);
-//                System.out.println(res3);
-
-                // 55555555
-                // Read Namespace Array
-//                ReadResponse res5 = mySession.Read(null, null, TimestampsToReturn.Neither,
-//                        new ReadValueId(Identifiers.Server_NamespaceArray, Attributes.Value, null, null));
-//                String[] namespaceArray = (String[]) res5.getResults()[0].getValue().getValue();
-//                System.out.println(Arrays.toString(namespaceArray));
-
-                //////////// TEST-STACK ////////////
-                // Create Channel
-//                ServiceChannel myChannel = myClient.createServiceChannel(endpoint);
-//                // Create Test Request
-//                ReadValueId[] nodesToRead = {new ReadValueId(Identifiers.RootFolder, Attributes.BrowseName, null, null)};
-//                ReadRequest req = new ReadRequest(null, 0.0, TimestampsToReturn.Both, nodesToRead);
-//                System.out.println("REQUEST: " + req);
-//
-//                // Invoke service
-//                ReadResponse res = myChannel.Read(req);
-//                // Print result
-//                System.out.println("RESPONSE: " + res);
-                //////////////////////////////////////
-
+                mySession.Write(null, new WriteValue(new NodeId(5, "Counter1"), Attributes.Value, null, new DataValue(new Variant(0))));
 
                 mySession.close();
                 mySession.closeAsync();
-
 
             } catch (Exception e) {
                 e.printStackTrace();
